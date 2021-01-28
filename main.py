87e9208c896ec60014a16235f917b54b504ae6c9
BetoSkey <gilberto_diaz_aguilar@hotmail.com>
2021-01-27 21:09:09 -0600

    Se elimina archivo main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from programacion_dinamica import fibonacci_recursivo
-
-if __name__ == '__main__':
-  # Prueba fibonacci recursivo
-  '''n = int(input('Escoge un numero: '))
-  resultado = fibonacci_recursivo(n)
-  print(resultado)'''
-  
