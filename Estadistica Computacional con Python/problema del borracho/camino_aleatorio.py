aab3fa666a21b772219f7998d31f9da490004db1
BetoSkey <gilberto_diaz_aguilar@hotmail.com>
2021-01-30 01:58:20 -0600

    se agrega gitignore, camino alearorio, campo y la grafica

--- Estadistica Computacional con Python/problema del borracho/camino_aleatorio.py
+++ Estadistica Computacional con Python/problema del borracho/camino_aleatorio.py
@@ -1,54 +1,77 @@
 from borracho import BorrachoTradicional
 from campo import Campo
 from coordenada import Coordenada
+from bokeh.plotting import figure, show
+
 
 def caminata(campo, borracho, pasos):
-  # Nos da las coordenadas iniciales del borracho
-  inicio = campo.obtener_coordenada(borracho)
+    # Nos da las coordenadas iniciales del borracho
+    inicio = campo.obtener_coordenada(borracho)
 
-  # Por cada paso nos da la nueva coordenada del borracho simulando que se mueve de lugar
-  for _ in range(pasos):
-    campo.mover_borracho(borracho)
-  
-  # Regresa la distancia entre las coordenadas de inicio y la ultima coordenada del borracho
-  return inicio.distancia(campo.obtener_coordenada(borracho))
+    # Por cada paso nos da la nueva coordenada del borracho simulando que se mueve de lugar
+    for _ in range(pasos):
+        campo.mover_borracho(borracho)
+
+    # Regresa la distancia entre las coordenadas de inicio y la ultima coordenada del borracho
+    return inicio.distancia(campo.obtener_coordenada(borracho))
+
 
 def simular_caminata(pasos, numero_de_intentos, tipo_de_borracho):
-  # Crea una instancia de un borracho
-  borracho = tipo_de_borracho(nombre='David')
-  # Crea una instancia de coordenada
-  origen = Coordenada(0, 0)
-  #Lista para almacenar las distancias en cada movimiento del borracho
-  distancias = []
+    # Crea una instancia de un borracho
+    borracho = tipo_de_borracho(nombre='David')
+    # Crea una instancia de coordenada
+    origen = Coordenada(0, 0)
+    # Lista para almacenar las distancias en cada movimiento del borracho
+    distancias = []
 
-  for _ in range(numero_de_intentos):
-    # Se crea una instancia de campo que nos regresa las coordenadas de los borrachos
-    campo = Campo()
-    # Se añade el borracho con su coordenada de origen al campo
-    campo.anadir_borracho(borracho, origen)
-    # Nos regresa la distancia de caminata del borracho a la cantidad de pasos
-    simulacion_caminata = caminata(campo, borracho, pasos)
-    # Se agrega la distancia a la lista de distancias redondeandolo para que no tenga ningun decimal
-    distancias.append(round(simulacion_caminata, 1))
+    for _ in range(numero_de_intentos):
+        # Se crea una instancia de campo que nos regresa las coordenadas de los borrachos
+        campo = Campo()
+        # Se añade el borracho con su coordenada de origen al campo
+        campo.anadir_borracho(borracho, origen)
+        # Nos regresa la distancia de caminata del borracho a la cantidad de pasos
+        simulacion_caminata = caminata(campo, borracho, pasos)
+        # Se agrega la distancia a la lista de distancias redondeandolo para que no tenga ningun decimal
+        distancias.append(round(simulacion_caminata, 1))
 
-  return distancias
-    
+    return distancias
+
+
+def graficar(x, y):
+    f = figure(
+        title='Camino aleatorio',
+        x_axis_label='pasos',
+        y_axis_label='distancia')
+
+    f.circle(x, y, legend='distancia media')
+
+    show(f)
+
+
 def main(distancias_de_caminata, numero_de_intentos, tipo_de_borracho):
+    distancias_medias_por_caminata = []
 
-  for pasos in distancias_de_caminata:
-    
-    distancias = simular_caminata(pasos, numero_de_intentos, tipo_de_borracho)
-    # Calcular los datos estadisticos
-    distancia_media = round(sum(distancias) / len(distancias), 4)
-    distancia_maxima = max(distancias)
-    distancia_minima = min(distancias)
-    print(f'{tipo_de_borracho.__name__} caminata aleatoria de {pasos} pasos')
-    print(f'Media = {distancia_media}')
-    print(f'Max = {distancia_maxima}')
-    print(f'Min = {distancia_minima}')
+    for pasos in distancias_de_caminata:
+        distancias = simular_caminata(
+            pasos, numero_de_intentos, tipo_de_borracho)
+        print(distancias)
+        # Calcular los datos estadisticos
+        distancia_media = round(sum(distancias) / len(distancias), 4)
+        distancia_maxima = max(distancias)
+        distancia_minima = min(distancias)
+
+        distancias_medias_por_caminata.append(distancia_media)
+
+        print(f'{tipo_de_borracho.__name__} caminata aleatoria de {pasos} pasos')
+        print(f'Media = {distancia_media}')
+        print(f'Max = {distancia_maxima}')
+        print(f'Min = {distancia_minima}')
+
+    graficar(x=distancias_de_caminata, y=distancias_medias_por_caminata)
+
 
 if __name__ == '__main__':
-  distancias_de_caminata = [10, 100, 1000, 10000]
-  numero_de_intentos = 100
+    distancias_de_caminata = [10, 100, 1000, 10000]
+    numero_de_intentos = 100
 
-  main(distancias_de_caminata, numero_de_intentos, BorrachoTradicional)
+    main(distancias_de_caminata, numero_de_intentos, BorrachoTradicional)
--- Estadistica Computacional con Python/problema del borracho/campo.py
+++ Estadistica Computacional con Python/problema del borracho/campo.py
@@ -1,24 +1,25 @@
 
 class Campo:
 
-  def __init__(self):
-    self.coordenadas_de_borrachos = {}
-  
-  def anadir_borracho(self, borracho, coordenada):
-    # Añade el borracho y su coordenada al diccionario 'coordenadas de borrachos {borracho:coordenada}'
-    self.coordenadas_de_borrachos[borracho] = coordenada
-  
-  def mover_borracho(self, borracho):
-    # Se llama a la funcion camina del borracho para obtener aleatoriamente una nueva tupla que se asigna a 'delta_x' y 'delta_y'
-    delta_x, delta_y = borracho.camina()
-    # coordenada actual nos trae del diccionario las coordenadas del borracho
-    coordenada_actual = self.coordenadas_de_borrachos[borracho]
-    # nueva coordenada utiliza el metodo mover de la clase coordenada y recibe las coordenadas del metodo camina del borracho
-    # nos da una nueva coordenada sumando la actual con la nueva
-    nueva_coordenada = coordenada_actual.mover(delta_x, delta_y)
+    def __init__(self):
+        self.coordenadas_de_borrachos = {}
 
-    # asigna la nueva coordenada al mismo borracho dentro del diccionario de coordenadas de borrachos
-    self.coordenadas_de_borrachos[borracho] = nueva_coordenada
-  
-  def obtener_coordenada(self, borracho):
-    return self.coordenadas_de_borrachos[borracho]
+    def anadir_borracho(self, borracho, coordenada):
+        # Añade el borracho y su coordenada al diccionario 'coordenadas de borrachos {borracho:coordenada}'
+        self.coordenadas_de_borrachos[borracho] = coordenada
+
+    def mover_borracho(self, borracho):
+        # Se llama a la funcion camina del borracho para obtener aleatoriamente una nueva coordenada en
+        # tupla que se asigna a 'delta_x' y 'delta_y'
+        delta_x, delta_y = borracho.camina()
+        # coordenada actual nos trae del diccionario las coordenadas del borracho
+        coordenada_actual = self.coordenadas_de_borrachos[borracho]
+        # nueva coordenada utiliza el metodo mover de la clase coordenada y recibe las coordenadas del metodo camina del borracho
+        # nos da una nueva coordenada sumando la actual con la nueva
+        nueva_coordenada = coordenada_actual.mover(delta_x, delta_y)
+
+        # asigna la nueva coordenada al mismo borracho dentro del diccionario de coordenadas de borrachos
+        self.coordenadas_de_borrachos[borracho] = nueva_coordenada
+
+    def obtener_coordenada(self, borracho):
+        return self.coordenadas_de_borrachos[borracho]
