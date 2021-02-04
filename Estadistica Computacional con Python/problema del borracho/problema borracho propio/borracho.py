d5cfd8d7a92529fe280e1f610942dde083026bb2
BetoSkey <gilberto_diaz_aguilar@hotmail.com>
2021-02-03 23:49:10 -0600

    Se continua con los apuntes de Estadistica Computacional

--- Estadistica Computacional con Python/problema del borracho/problema borracho propio/borracho.py
+++ Estadistica Computacional con Python/problema del borracho/problema borracho propio/borracho.py
@@ -8,10 +8,11 @@ class Borracho:
     * Recibir ubicacion inicial
     * Caminar al borracho
     * Calcular la distancia que se alejo desde el inicio'''
+
     def __init__(self, ubicacion_inicial=(0, 0)):
         self.ubicacion_inicial = ubicacion_inicial
         self.ubicaciones = [self.ubicacion_inicial]
-    
+
     def obtener_nueva_coordenada(self):
         '''Obtiene aleatoriamente una nueva coordenada con la misma probabilidad hacia los 4 puntos cardinales.'''
         nueva_coordenada = random.choice([(1, 0), (0, 1), (-1, 0), (0, -1)])
@@ -20,10 +21,11 @@ class Borracho:
     def mover_borracho(self):
         '''Mueve al borracho hacia la nueva ubicacion aleatoria
         generada y agrega su nueva ubicacion a la lista de ubicaciones de su trayectoria.'''
-        nueva_ubicacion = (self.ubicaciones[-1][0] +
-                           self.obtener_nueva_coordenada()[0],
-                           +self.ubicaciones[-1][1] +
-                           self.obtener_nueva_coordenada()[1])
+        nueva_ubicacion = (
+            self.ubicaciones[-1][0] +
+            self.obtener_nueva_coordenada()[0],
+            +self.ubicaciones[-1][1] +
+            self.obtener_nueva_coordenada()[1])
         self.ubicaciones.append(nueva_ubicacion)
 
     def caminar_borracho(self, pasos):
@@ -52,6 +54,7 @@ class Borracho_crudo(Borracho):
     '''Este borracho caminara aleatoriamente y con la misma probabilidad,
     un paso hascia cualquier punto cardinal.
     * Puede recibir una ubicacion inicial'''
+
     def __init__(self, ubicacion_inicial):
         super().__init__(ubicacion_inicial)
 
@@ -64,20 +67,24 @@ class Borracho_drogado(Borracho):
     '''Este borracho caminara aleatoriamente un rango entre 0 y 10 con la misma
     probabilidad en cualquier punto cardinal.
     * Puede recibir una ubicacion inicial'''
+
     def __init__(self, ubicacion_inicial):
         super().__init__(ubicacion_inicial)
 
     def obtener_nueva_coordenada(self):
-        nueva_coordenada = random.choice([(random.randint(0, 10), 0),
-                                          (0, random.randint(0, 10)),
-                                          (random.randint(-10, 0), 0),
-                                          (0, random.randint(-10, 0))])
+        nueva_coordenada = random.choice([
+            (random.randint(0, 10), 0),
+            (0, random.randint(0, 10)),
+            (random.randint(-10, 0), 0),
+            (0, random.randint(-10, 0))
+        ])
         return nueva_coordenada
 
 
 class Prueba_estocastica:
     '''La prueba estocastica camina al borracho por cada intento, el numero de pasos indicado.
     * Nos realiza un analisis estadistico de las distancias recorridas por cada intento.'''
+
     def __init__(self, borracho, intentos, pasos):
         self.borracho = borracho
         self.intentos = intentos
@@ -100,13 +107,15 @@ class Prueba_estocastica:
         self.distancia_maxima = max(self.distancias)
 
     def analisis_estadistico(self):
-        print('--------------------------------------------\n',
-              f'Numero de pasos del borracho: {self.pasos}\n',
-              f'Intentos: {self.intentos}\n',
-              f'Distancias de cada intento: {self.distancias}\n',
-              f'Distancia Minima: {self.distancia_minima}\n',
-              f'Distancia Media: {self.distancia_media}\n',
-              f'Distancia maxima: {self.distancia_maxima}')
+        print(
+            '--------------------------------------------\n',
+            f'Numero de pasos del borracho: {self.pasos}\n',
+            f'Intentos: {self.intentos}\n',
+            f'Distancias de cada intento: {self.distancias}\n',
+            f'Distancia Minima: {self.distancia_minima}\n',
+            f'Distancia Media: {self.distancia_media}\n',
+            f'Distancia maxima: {self.distancia_maxima}'
+        )
 
 
 if __name__ == '__main__':
@@ -116,5 +125,5 @@ if __name__ == '__main__':
     prueba1 = Prueba_estocastica(borracho1, intentos, pasos_del_borracho)
     prueba1.correr_prueba()
     prueba1.analisis_estadistico()
-    #borracho1.caminar_borracho(pasos_del_borracho)
-    #borracho1.graficar_caminata()
+    borracho1.caminar_borracho(pasos_del_borracho)
+    borracho1.graficar_caminata()
