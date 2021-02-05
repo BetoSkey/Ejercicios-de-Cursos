3fefb767abe644daafab585fff8e2c4e4da107cd
BetoSkey <gilberto_diaz_aguilar@hotmail.com>
2021-02-05 01:46:32 -0600

    se logran obtener probabilidades totales promediadas

--- Estadistica Computacional con Python/tirar_dados.py
+++ Estadistica Computacional con Python/tirar_dados.py
@@ -1,37 +1,58 @@
 import random
 
-def tirar_dado(): 
-  resultado = random.randint(1,6)
-  return resultado
+
+def tirar_dado():
+    resultado = random.randint(1, 6)
+    return resultado
+
 
 def main(dados, tiros, repeticiones):
-  
-  for repeticion in range(repeticiones):
-    probabilidad_x_repeticion ={}
-    
-    for tiro in range(tiros):
-      lista_numeros_obtenidos = []
-      
-      for dado in range(dados):
-        numero_obtenido = tirar_dado()
-        lista_numeros_obtenidos.append(numero_obtenido)
-      
-      probabilidad_x_tiro = dict(zip([num for num in range(1,7)], [lista_numeros_obtenidos.count(num)/len(lista_numeros_obtenidos)for num in range(1, 7)]))
-      
-      print(probabilidad_x_tiro)
-      '''Formula para sumar dos diccionarios:
-      new_dict = {}
-      for key, value in dict1.items():
-        sum_array = [(dict1[key][i] + dict2[key][i]) for i, ele in enumerate(value)]
-        new_dict.setdefault(key, sum_array)'''
+    '''Simulacion de tiro de dados'''
 
-      #probabilidad_x_repeticion = dict(zip([num for num in range(1,7)],[lista_numeros_obtenidos.count(num)/len(lista_numeros_obtenidos) for num in range(1,7)]))
+    # Pör cada repeticion
+    for repeticion in range(repeticiones):
+        probabilidad_x_repeticion = dict(zip(
+            [num for num in range(1, 7)],
+            [[None] for num in range(1, 7)]
+        ))
 
-  return probabilidad_x_repeticion
+        # Por cada tiro
+        for tiro in range(tiros):
+            lista_numeros_obtenidos = []
+
+            # Por cada dado
+            for dado in range(dados):
+                numero_obtenido = tirar_dado()
+                lista_numeros_obtenidos.append(numero_obtenido)
+
+            probabilidad_x_tiro = dict(zip(
+                [num for num in range(1, 7)],
+                [lista_numeros_obtenidos.count(
+                    num)/len(lista_numeros_obtenidos) for num in range(1, 7)]
+            ))
+
+            # Agregar probabilidades de cada numero obtenidas por cada tiro, a cada intento:
+            for key, value in probabilidad_x_tiro.items():
+
+                if probabilidad_x_repeticion[key] == [None]:
+                    probabilidad_x_repeticion[key] = [value]
+                else:
+                    probabilidad_x_repeticion[key].append(value)
+
+        # Promedio de las probabilidades totales obtenidas por cada numero
+        for key in probabilidad_x_repeticion:
+            average = round(sum(probabilidad_x_repeticion[key]) /
+                            len(probabilidad_x_repeticion[key]), 3)
+            probabilidad_x_repeticion[key] = average
+
+    return probabilidad_x_repeticion
+
 
 if '__main__' == __name__:
-  dados = int(input('Cuantos dados: '))
-  tiros = int(input('Cuantos tiros: '))
-  repeticiones = int(input('Cuantas repeticiones: '))
+    dados = int(input('Cuantos dados: '))
+    tiros = int(input('Cuantos tiros: '))
+    repeticiones = int(input('Cuantas repeticiones: '))
 
-  print(main(dados, tiros, repeticiones))
+    prueba = main(dados, tiros, repeticiones)
+    for key, value in prueba.items():
+        print(f'Probabilidad de {key}: {value}')
